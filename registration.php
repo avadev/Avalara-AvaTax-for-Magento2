0375cb615dfd3390b58efb861927f23f42f8865a
Jonathan Hodges <jonathan@classyllama.com>
2015-10-14 23:20:34 -0500

    #401_24 - Register avatax library from classyllama module

--- registration.php
+++ registration.php
@@ -5,3 +5,9 @@
     'ClassyLlama_AvaTax',
     __DIR__
 );
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+    'Avalara_AvaTax',
+    __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'avalara' . DIRECTORY_SEPARATOR . 'avatax'
+);
