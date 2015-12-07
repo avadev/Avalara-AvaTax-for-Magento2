10dc979815575fda3a270842d3ee8fa5a1d13507
Nathan Toombs <nathan.toombs@classyllama.com>
2015-12-07 13:16:23 -0600

    #401_29 - Added code to initial ShippingAddress api request - Added same code inside the response of the api request for both default.js and gift-registry.js. Both of these files can be potentially run to submit the request and recieve the response so the logic must be handled in both functions.

---
+++ view/frontend/web/js/model/shipping-save-processor/default.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*global define,alert*/
+define(
+    [
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/resource-url-manager',
+        'mage/storage',
+        'Magento_Checkout/js/model/payment-service',
+        'Magento_Checkout/js/model/payment/method-converter',
+        'Magento_Checkout/js/model/error-processor',
+        'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Checkout/js/action/select-billing-address',
+        'ClassyLlama_AvaTax/js/view/validation'
+    ],
+    function (
+        quote,
+        resourceUrlManager,
+        storage,
+        paymentService,
+        methodConverter,
+        errorProcessor,
+        fullScreenLoader,
+        selectBillingAddressAction,
+        validation
+    ) {
+        'use strict';
+
+        return {
+            saveShippingInformation: function () {
+                var payload;
+
+                if (!quote.billingAddress()) {
+                    selectBillingAddressAction(quote.shippingAddress());
+                }
+
+                payload = {
+                    addressInformation: {
+                        shipping_address: quote.shippingAddress(),
+                        billing_address: quote.billingAddress(),
+                        shipping_method_code: quote.shippingMethod().method_code,
+                        shipping_carrier_code: quote.shippingMethod().carrier_code
+                    }
+                };
+
+                fullScreenLoader.startLoader();
+
+                return storage.post(
+                    resourceUrlManager.getUrlForSetShippingInformation(quote),
+                    JSON.stringify(payload)
+                ).done(
+                    function (response) {
+                        quote.setTotals(response.totals);
+                        paymentService.setPaymentMethods(methodConverter(response.payment_methods));
+                        // Begin Edit - Nathan Toombs <nathan.toombs@classyllama.com>
+                        validation.shippingInformationResponseHandeler(response);
+                        // End Edit
+                        fullScreenLoader.stopLoader();
+                    }
+                ).fail(
+                    function (response) {
+                        errorProcessor.process(response);
+                        fullScreenLoader.stopLoader();
+                    }
+                );
+            }
+        };
+    }
+);
---
+++ view/frontend/web/js/model/shipping-save-processor/gift-registry.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*global define,alert*/
+define(
+    [
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/resource-url-manager',
+        'mage/storage',
+        'Magento_Checkout/js/model/payment-service',
+        'Magento_Checkout/js/model/payment/method-converter',
+        'Magento_Checkout/js/model/error-processor',
+        'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Checkout/js/action/select-billing-address',
+        'ClassyLlama_AvaTax/js/view/validation'
+    ],
+    function (
+        quote,
+        resourceUrlManager,
+        storage,
+        paymentService,
+        methodConverter,
+        errorProcessor,
+        fullScreenLoader,
+        selectBillingAddressAction,
+        validation
+    ) {
+        'use strict';
+        return {
+            saveShippingInformation: function() {
+                var shippingAddress = {},
+                payload;
+
+                shippingAddress.extension_attributes = {
+                    gift_registry_id: quote.shippingAddress().giftRegistryId
+                };
+
+                payload = {
+                    addressInformation: {
+                        shipping_address: shippingAddress,
+                        shipping_method_code: quote.shippingMethod().method_code,
+                        shipping_carrier_code: quote.shippingMethod().carrier_code
+                    }
+                };
+
+                fullScreenLoader.startLoader();
+                return storage.post(
+                    resourceUrlManager.getUrlForSetShippingInformation(quote),
+                    JSON.stringify(payload)
+                ).done(
+                    function (response) {
+                        paymentService.setPaymentMethods(methodConverter(response.payment_methods));
+                        quote.setTotals(response.totals);
+                        // Begin Edit - Nathan Toombs <nathan.toombs@classyllama.com>
+                        validation.shippingInformationResponseHandeler(response);
+                        // End Edit
+                        fullScreenLoader.stopLoader();
+                    }
+                ).fail(
+                    function (response) {
+                        errorProcessor.process(response);
+                        fullScreenLoader.stopLoader();
+                    }
+                );
+            }
+        }
+    }
+);
