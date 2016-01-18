b355f5328badfe66b8b14cb4cec29ec27a2a987b
Erik Hansen <erikpallhansen@gmail.com>
2016-01-18 17:16:04 -0600

    #401_24 - Add debugging code for GetTax request to Git history
    - Will reverse this commit since this could should not exist in module

--- Framework/Interaction/Tax/Get.php
+++ Framework/Interaction/Tax/Get.php
@@ -221,6 +221,7 @@ class Get
         try {
             $getTaxResult = $taxService->getTax($getTaxRequest, true);
             if ($getTaxResult->getResultCode() == \AvaTax\SeverityLevel::$Success) {
+                $this->extraDebug($getTaxRequest, $getTaxResult, $quote);
 
                 $store = $quote->getStore();
                 $taxDetails =
@@ -285,5 +286,157 @@ class Get
         }
 
         return $message;
+    }
+
+    /**
+     * Debug get tax request/response and compare to quote or invoice/credit memo and compare results
+     *
+     * @param $getTaxRequest
+     * @param $getTaxResult
+     * @param $data
+     * @return string
+     */
+    protected function extraDebug($getTaxRequest, $getTaxResult, $data)
+    {
+        /* @var \Magento\Quote\Model\Quote $data */
+        $log = '';
+        $log .= PHP_EOL . print_r($getTaxRequest, true);
+        $log .= PHP_EOL . print_r($getTaxResult, true);
+
+        $usdToEurExchangeRate = 2;
+        $taxDump = '##############' . PHP_EOL;
+        $taxDump .= '### Result ###' . PHP_EOL;
+        $taxDump .= '##############' . PHP_EOL;
+        $totalTaxable = 0;
+        $itemsTaxable = 0;
+        $itemsTax = 0;
+        $nonItemsTaxable = 0;
+        $nonItemsTax = 0;
+        $totalTax = 0;
+
+        $items = $data->getItems();
+        if (is_null($items)) {
+            if (method_exists($data, 'getItemsCollection')) {
+                $items = $data->getItemsCollection()->count() > 0 ? $data->getItemsCollection() : null;
+            }
+            if (is_null($items)) {
+                $items = [];
+            }
+        }
+
+        foreach ($items as $item) {
+            $itemsByTaxCalculationItemId[$item->getTaxCalculationItemId()] = $item;
+        }
+        foreach ($items as $item) {
+            $itemsById[$item->getId()] = $item;
+        }
+
+        /* @var \AvaTax\Line $taxLine */
+        foreach ($getTaxResult->getTaxLines() as $taxLine) {
+            $calculateTowardsTotals = true;
+            if (
+                isset($itemsByTaxCalculationItemId[$taxLine->getNo()])
+                && $itemsByTaxCalculationItemId[$taxLine->getNo()]->getProduct()->getTypeId() == 'bundle'
+            ) {
+                $calculateTowardsTotals = false;
+            }
+
+            if ($calculateTowardsTotals) {
+                $totalTaxable += (float)$taxLine->getTaxable();
+                if (is_numeric($taxLine->getNo())) {
+                    $itemsTaxable += (float)$taxLine->getTaxable();
+                    $itemsTax += (float)$taxLine->getTax();
+                } else {
+                    $nonItemsTaxable += (float)$taxLine->getTaxable();
+                    $nonItemsTax += (float)$taxLine->getTax();
+                }
+                $totalTax += (float)$taxLine->getTax();
+            }
+            // TODO: Add check for dynamically priced bundle vs fixed
+            if (!$calculateTowardsTotals) {
+                $taxDump .= 'This line not included in totals since it\'s a bundle' . PHP_EOL;
+            }
+            $taxDump .= $taxLine->getNo() . PHP_EOL;
+            $taxDump .= "   Taxable: " . $taxLine->getTaxable() . ' | ' . ($usdToEurExchangeRate * $taxLine->getTaxable()). PHP_EOL;
+            $taxDump .= "   TaxCode: " . $taxLine->getTaxCode() . PHP_EOL;
+            $taxDump .= "   Rate: " . $taxLine->getRate() . PHP_EOL;
+            $taxDump .= "   Tax: " . $taxLine->getTax() . ' | ' . ($usdToEurExchangeRate * $taxLine->getTax()) . PHP_EOL;
+
+            if (isset($itemsById[$taxLine->getNo()])) {
+                $item = $itemsById[$taxLine->getNo()];
+                $taxDump .= "   Magento Tax: " . $item->getBaseTaxAmount() . ' | ' . ($usdToEurExchangeRate * $item->getBaseTaxAmount()) . PHP_EOL;
+
+                if ((float)abs($taxLine->getTax()) != (float)abs($item->getBaseTaxAmount())) {
+                    $diff = (float)abs($taxLine->getTax()) - (float)abs($item->getBaseTaxAmount());
+                    $taxDump .= '   #########################################' . PHP_EOL;
+                    $taxDump .= '   #########################################' . PHP_EOL;
+                    $taxDump .= "   ###########    Diff of $diff   ##########" . PHP_EOL;
+                    $taxDump .= '   #########################################' . PHP_EOL;
+                    $taxDump .= '   #########################################' . PHP_EOL;
+                }
+            }
+
+            if (isset($itemsById[$taxLine->getNo()])) {
+                $discount = $itemsById[$taxLine->getNo()]->getBaseDiscountAmount();
+            } else {
+                $discount = 0;
+            }
+            $taxDump .= "   Discount: " . $discount . ' | ' . ($usdToEurExchangeRate * $discount) . PHP_EOL;
+
+            $taxDump .= "   Total Row: " . ($taxLine->getTaxable() + $taxLine->getTax() + $discount) . ' | ' . ($usdToEurExchangeRate * ($taxLine->getTaxable() + $taxLine->getTax() + $discount)) . PHP_EOL;
+            $taxDump .= "   Total Row (Excl Tax): " . ($taxLine->getTaxable() + $discount) . ' | ' . ($usdToEurExchangeRate * ($taxLine->getTaxable() + $discount)) . PHP_EOL;
+            $taxDump .= PHP_EOL . PHP_EOL;
+        }
+
+        $itemsDiscountAmount = 0;
+
+        foreach ($items as $item) {
+            if ($item->getProduct() && $item->getProduct()->getTypeId() == 'bundle') {
+                continue;
+            }
+            $itemsDiscountAmount += $item->getBaseDiscountAmount();
+        }
+        $shippingDiscountAmount = $data->getShippingAddress()->getBaseShippingDiscountAmount();
+
+
+        $taxDump .= 'Total Taxable: ' . $totalTaxable . ' | ' . ($usdToEurExchangeRate * $totalTaxable) . PHP_EOL;
+        $taxDump .= '    Items Taxable: ' . $itemsTaxable . ' | ' . ($usdToEurExchangeRate * $itemsTaxable) . PHP_EOL;
+        $taxDump .= '    Non-Items Taxable (gw, shipping): ' . $nonItemsTaxable . ' | ' . ($usdToEurExchangeRate * $nonItemsTaxable) . PHP_EOL;
+        $taxDump .= 'Total Discount: ' . ($itemsDiscountAmount + $shippingDiscountAmount) . ' | ' . ($usdToEurExchangeRate * ($itemsDiscountAmount + $shippingDiscountAmount)) . PHP_EOL;
+        $taxDump .= '    Items Discount: ' . $itemsDiscountAmount . ' | ' . ($usdToEurExchangeRate * $itemsDiscountAmount) . PHP_EOL;
+        $taxDump .= '    Shipping Discount: ' . $shippingDiscountAmount . ' | ' . ($usdToEurExchangeRate * $shippingDiscountAmount) . PHP_EOL;
+        $taxDump .= 'Taxable + Discount: ' . ($totalTaxable + $itemsDiscountAmount + $shippingDiscountAmount) . ' | ' . ($usdToEurExchangeRate * ($totalTaxable + $itemsDiscountAmount + $shippingDiscountAmount)) . PHP_EOL;
+
+
+
+        $taxDump .= 'Total Tax: ' . $totalTax . ' | ' . ($usdToEurExchangeRate * $totalTax) . PHP_EOL;
+        $taxDump .= '    Items Tax: ' . $itemsTax . ' | ' . ($usdToEurExchangeRate * $itemsTax) . PHP_EOL;
+        $taxDump .= '    Non-Items Tax (gw, shipping): ' . $nonItemsTax . ' | ' . ($usdToEurExchangeRate * $nonItemsTax) . PHP_EOL . PHP_EOL;
+
+        $taxDump .= 'Magento Total Tax: ' . $data->getBaseTaxAmount() . ' | ' . $data->getTaxAmount() . PHP_EOL . PHP_EOL;
+        if ((float)abs($totalTax) != (float)abs($data->getBaseTaxAmount())) {
+            $diff = (float)abs($totalTax) - (float)abs($data->getBaseTaxAmount());
+            $taxDump .= '   #########################################' . PHP_EOL;
+            $taxDump .= '   #########################################' . PHP_EOL;
+            $taxDump .= "   ###########    Diff of $diff   ##########" . PHP_EOL;
+            $taxDump .= '   #########################################' . PHP_EOL;
+            $taxDump .= '   #########################################' . PHP_EOL;
+        } else {
+            $taxDump .= '   #########################################' . PHP_EOL;
+            $taxDump .= "   ###########    Tax Matches!!   ##########" . PHP_EOL;
+            $taxDump .= '   #########################################' . PHP_EOL;
+        }
+
+
+        $taxDump .= 'Cart Subtotal Excl. Tax: ' . ($itemsTaxable + $itemsDiscountAmount) . ' | ' . ($usdToEurExchangeRate * ($itemsTaxable + $itemsDiscountAmount))  . PHP_EOL;
+        $taxDump .= 'Cart Subtotal Incl. Tax: ' . ($itemsTaxable + $itemsDiscountAmount + $itemsTax) . ' | ' . ($usdToEurExchangeRate * ($itemsTaxable + $itemsDiscountAmount + $itemsTax))  . PHP_EOL;
+        $taxDump .= 'Order Total: ' . ($itemsTaxable + + $nonItemsTaxable + $itemsDiscountAmount + $itemsTax + $nonItemsTax)  . PHP_EOL;
+        // NOT accurate
+        //$taxDump .= 'Discount: ' . ($itemsTaxable + $itemsDiscountAmount + $itemsTax)  . PHP_EOL;
+
+        $log .= $taxDump;
+
+        file_put_contents(BP . '/var/log/avatax.log', PHP_EOL . $log, FILE_APPEND);
+        return $log;
     }
 }
