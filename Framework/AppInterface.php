6cec0a5f11d42b3baff99242c95de6f263d5dae9
Erik Hansen <erikpallhansen@gmail.com>
2016-01-22 12:37:24 -0600

    #401_24 - Update module version

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -19,5 +19,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in etc/module.xml in the config->module@setup_version attribute
      */
-    const APP_VERSION = '0.1.0';
+    const APP_VERSION = '0.1.4';
 }
