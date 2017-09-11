f8b102953dc09f07248869b2b469e805ecbc5a83
Erik Hansen <erikpallhansen@gmail.com>
2017-09-10 21:17:21 -0500

    Fix registration.php so it works with integration tests

--- registration.php
+++ registration.php
@@ -6,8 +6,16 @@
     __DIR__
 );
 
+if (defined('BP')) {
+    // This path will work when extension is installed via composer or via manual installation
+    $vendorPathPrefix = BP . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR;
+} else {
+    // This path will work when extension is being run in the context of integration tests, where BP is not defined
+    $vendorPathPrefix = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+}
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LIBRARY,
     'Avalara_AvaTax',
-    BP . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'avalara' . DIRECTORY_SEPARATOR . 'avatax'
+    $vendorPathPrefix . 'avalara' . DIRECTORY_SEPARATOR . 'avatax'
 );
