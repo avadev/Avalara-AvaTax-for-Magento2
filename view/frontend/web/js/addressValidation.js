bf9448aa8814a9975936d6af8420bef49a8a1d68
Richard Sisco <richard.sisco@classyllama.com>
2017-11-24 15:34:27 -0600

    AVS-421_67 - Revise for Magento 2.2 compatibility
    - Refactor code to be fully compatible with Magento 2.2 while retaining
      backwards compatibility with 2.1
    - Addresses Issue #85

---
+++ view/frontend/web/js/addressValidation.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'jquery/ui',
+    'validation'
+], function ($) {
+    'use strict';
+
+    $.widget('mage.addressValidation', {
+        options: {
+            selectors: {
+                button: '[data-action=save-address]'
+            }
+        },
+
+        /**
+         * Validation creation
+         * @protected
+         */
+        _create: function () {
+            var button = $(this.options.selectors.button, this.element);
+
+            this.element.validation({
+
+                /**
+                 * Submit Handler
+                 * @param {Element} form - address form
+                 */
+                submitHandler: function (form) {
+
+                    button.attr('disabled', true);
+                    // BEGIN EDIT - Remove form submit
+                    // END EDIT
+                }
+            });
+        }
+    });
+
+    return $.mage.addressValidation;
+});
