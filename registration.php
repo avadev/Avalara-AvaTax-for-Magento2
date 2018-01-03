98fde54752e414eeda2d61217e813c96065d35ef
makao <m.makalowski@gmail.com>
2018-01-03 11:47:30 +0100

    Fixed BP constant issue in registration.php

--- registration.php
+++ registration.php
@@ -9,5 +9,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LIBRARY,
     'Avalara_AvaTax',
-    BP . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'avalara' . DIRECTORY_SEPARATOR . 'avatax'
+    __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'avalara' . DIRECTORY_SEPARATOR . 'avatax'
 );
