11e8519fb147e6aa529ba495db870f6e38ffc1d8
Chris Nanninga <chris.nanninga@classyllama.com>
2018-04-13 16:20:49 -0500

    Added back in component registration for Avalara library, since class generation fails in 2.1

--- registration.php
+++ registration.php
@@ -5,3 +5,17 @@
     'ClassyLlama_AvaTax',
     __DIR__
 );
+
+if (defined('BP')) {
+    // This path will work when extension is installed via composer or via manual installation
+    $vendorPath = BP . DIRECTORY_SEPARATOR . 'vendor';
+} else {
+    // This path will work when extension is being run in the context of integration tests, where BP is not defined
+    $vendorPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+}
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+    'Avalara_AvaTax',
+    $vendorPath . DIRECTORY_SEPARATOR . 'avalara' . DIRECTORY_SEPARATOR . 'avataxclient'
+);
