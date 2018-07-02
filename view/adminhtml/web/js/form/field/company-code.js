d1176bb435409f33120ae6b5604065941e9fb253
Sean Templeton <sean@templeton.io>
2018-07-01 21:23:43 -0500

    Refactor to use Magento element

--- view/adminhtml/web/js/form/field/company-code.js
+++ view/adminhtml/web/js/form/field/company-code.js
@@ -1,79 +1,106 @@
-define(['jquery', 'mage/url'], function (jQuery) {
-    function getScope() {
-        var formScope = document.getElementById('config-edit-form').action.match(/section\/\w+\/(website|store)\/(\d+)/i);
+define(['jquery', 'uiElement', 'mage/url'], function (jQuery, Element) {
+    return Element.extend({
+        defaults: {
+            companyIdToCompanyCodeMap: [],
+            account_number_id: null,
+            license_key_id: null,
+            company_code_id: null
+        },
 
-        if (formScope === null) {
+        // Returns the scope from the form action to determine how to load the save config settings
+        getScope: function getScope() {
+            var formScope = document.getElementById('config-edit-form').action.match(/section\/\w+\/(website|store)\/(\d+)/i);
+
+            if (formScope === null) {
+                return {
+                    scope_type: 'global'
+                };
+            }
+
             return {
-                scope_type: 'global'
+                scope: formScope[2],
+                scope_type: formScope[1]
             };
-        }
+        },
 
-        return {
-            scope: formScope[2],
-            scope_type: formScope[1]
-        };
-    }
+        initialize: function initialize(config, idElement) {
+            var fetchAndUpdateCompanies = (function () {
+                this.fetchCompanies(this.accountNumberElement.value, this.licenseKeyElement.value)
+                    .then((function onCompanyFetch(response) {
+                        this.updateCompanyIds(response.companies, response.current_id)
+                    }).bind(this));
+            }).bind(this);
 
-    return function companyCode(config, idElement) {
-        var companyIdToCompanyCodeMap = [],
-            url = config.url,
-            accountNumberElement = document.getElementById(config.account_number_id),
-            licenseKeyElement = document.getElementById(config.license_key_id),
-            companyCodeElement = document.getElementById(config.company_code_id);
+            this._super();
 
-        if (accountNumberElement === null || licenseKeyElement === null || companyCodeElement === null) {
-            return;
-        }
+            this.idElement = idElement;
+            this.accountNumberElement = document.getElementById(this.account_number_id);
+            this.licenseKeyElement = document.getElementById(this.license_key_id);
+            this.companyCodeElement = document.getElementById(this.company_code_id);
 
-        function updateCompanyCode() {
-            companyCodeElement.value = companyIdToCompanyCodeMap[idElement.item(idElement.selectedIndex).value];
-        }
+            if (this.accountNumberElement === null || this.licenseKeyElement === null || this.companyCodeElement === null) {
+                return;
+            }
 
-        function updateCompanyId() {
+            // Watch for changes so we can provide instant company codes without the user needing to save the config
+            this.accountNumberElement.addEventListener('change', fetchAndUpdateCompanies);
+            this.licenseKeyElement.addEventListener('change', fetchAndUpdateCompanies);
+            this.idElement.addEventListener('change', this.updateCompanyCodeFromCompanyId.bind(this));
+
+            // If we already have values for credentials, fetch company ids
+            if (this.accountNumberElement.value !== null && this.licenseKeyElement.value !== null) {
+                fetchAndUpdateCompanies();
+            }
+        },
+
+        // Set the company code hidden input based on the selected company
+        updateCompanyCodeFromCompanyId: function updateCompanyCodeFromCompanyId() {
+            this.companyCodeElement.value = this.companyIdToCompanyCodeMap[this.idElement.item(this.idElement.selectedIndex).value];
+        },
+
+        // Build the company select, and select the currently saved company
+        updateCompanyIds: function updateCompanyIds(companies, currentId) {
+            this.idElement.innerHTML = '';
+
+            companies.forEach(function (company) {
+                var companyNameDisplay = company.name;
+
+                if (company.company_code !== null) {
+                    companyNameDisplay = company.company_code + ' - ' + companyNameDisplay;
+                }
+
+                this.companyIdToCompanyCodeMap[company.company_id] = company.company_code;
+                this.idElement.add(new Option(companyNameDisplay, company.company_id, false, company.company_id === currentId));
+                this.updateCompanyCodeFromCompanyId();
+            }.bind(this))
+        },
+
+        // Grab the companies from the API
+        fetchCompanies: function fetchCompanies(accountNumber, licenseKey) {
             // If either account number or license key is null, we can't make any request
-            if (accountNumberElement.value === '' || licenseKeyElement.value === '') {
+            if (accountNumber === '' || licenseKey === '') {
                 return jQuery.Deferred().reject();
             }
 
-            var data = getScope();
+            var data = this.getScope();
 
-            data['account_number'] = accountNumberElement.value;
+            data['account_number'] = accountNumber;
 
             // If license key is obscured, don't send it and use the saved config value
-            if (!RegExp("^[*]+$").test(licenseKeyElement.value)) {
-                data['license_key'] = licenseKeyElement.value;
+            if (!RegExp("^[*]+$").test(licenseKey)) {
+                data['license_key'] = licenseKey;
             }
 
             // We have to manually build the request and prevent native Magento's beforeSend handler,
             // otherwise the first request when the page loads doesn't work
-            jQuery.ajax({
-                url: url,
+            return jQuery.ajax({
+                url: this.url,
                 showLoader: true,
                 type: 'post',
                 data: data,
                 beforeSend: function () {
                 }
-            }).then(function (response) {
-                idElement.innerHTML = '';
-
-                response.companies.forEach(function (company) {
-                    companyIdToCompanyCodeMap[company.company_id] = company.company_code;
-                    idElement.add(new Option(company.company_code + ' - ' + company.name, company.company_id, false, company.company_id === response.current_id));
-                    updateCompanyCode();
-                })
             });
         }
-
-        if (accountNumberElement === null || licenseKeyElement === null || idElement === null) {
-            return;
-        }
-
-        accountNumberElement.addEventListener('change', updateCompanyId);
-        licenseKeyElement.addEventListener('change', updateCompanyId);
-        idElement.addEventListener('change', updateCompanyCode);
-
-        if (accountNumberElement.value !== null && licenseKeyElement.value !== null) {
-            updateCompanyId();
-        }
-    }
+    });
 });
