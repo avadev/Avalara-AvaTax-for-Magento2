4563c20d619e709a2ece5126f0df7259e6c9c232
Chris Nanninga <chris.nanninga@classyllama.com>
2018-07-20 15:58:11 -0500

    AVA-555_67 - Added cross border class repository getList, added ability to filter classes by country

---
+++ Api/SearchCriteria/CollectionProcessor/FilterProcessor/CrossBorderClass/CountryFilter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * ClassyLlama_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2018 Avalara, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+namespace ClassyLlama\AvaTax\Api\SearchCriteria\CollectionProcessor\FilterProcessor\CrossBorderClass;
+
+use Magento\Framework\Api\Filter;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessor\FilterProcessor\CustomFilterInterface;
+use Magento\Framework\Data\Collection\AbstractDb;
+
+class CountryFilter implements CustomFilterInterface
+{
+    /**
+     * Apply country filter to Cross Border Class collection
+     *
+     * @param Filter $filter
+     * @param AbstractDb $collection
+     * @return bool
+     */
+    public function apply(Filter $filter, AbstractDb $collection)
+    {
+        /** @var \ClassyLlama\AvaTax\Model\ResourceModel\CrossBorderClass\Collection $collection */
+        $collection->filterByCountries($filter->getValue());
+
+        return true;
+    }
+}
