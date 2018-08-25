39eefabaf36076b5ee52c7be5e78c72922350d7e
Sean Templeton <sean@templeton.io>
2018-08-25 13:42:41 -0500

    Update copyrights

--- view/frontend/web/js/view/estimation/mixin.js
+++ view/frontend/web/js/view/estimation/mixin.js
@@ -1,10 +1,15 @@
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-/**
- * @api
+ * ClassyLlama_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2018 Avalara, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 define(['Magento_Checkout/js/model/totals'], function (totals) {
