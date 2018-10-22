6ff484e95f91ade69bd7856de10d77dc8db50800
Stephen Biston <sjb9774@gmail.com>
2018-10-22 13:56:01 -0500

    AVS-421_82 - Update tax based on billing address

--- Observer/CalculateVirtualOrder.php
+++ Observer/CalculateVirtualOrder.php
@@ -7,39 +7,98 @@
 namespace ClassyLlama\AvaTax\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Checkout\Model\Session;
+use Magento\Checkout\Model\Session as QuoteSession;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
+use Magento\Quote\Model\QuoteRepository;
 
 class CalculateVirtualOrder implements ObserverInterface
 {
     /**
-     * @var Session
+     * @var QuoteSession
      */
-    protected $session;
+    protected $quoteSession;
 
     /**
-     * @param Session $session
+     * @var \Magento\Quote\Model\Quote\TotalsCollector
      */
-    public function __construct(Session $session)
-    {
-        $this->session = $session;
+    protected $totalsCollector;
+
+    /**
+     * @var CustomerRepository
+     */
+    protected $customerRepository;
+
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+
+    /**
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $quoteRepository;
+
+    /**
+     * @var \Magento\Quote\Model\ShippingAddressAssignment
+     */
+    protected $shippingAddressAssignment;
+
+    /** @var \Magento\Customer\Api\AddressRepositoryInterface */
+    protected $addressRepository;
+
+    /**
+     * @param QuoteSession                                   $quoteSession
+     * @param \Magento\Quote\Model\Quote\TotalsCollector     $totalsCollector
+     * @param CustomerRepository                             $customerRepository
+     * @param CustomerSession                                $customerSession
+     * @param QuoteRepository                                $quoteRepository
+     * @param \Magento\Quote\Model\ShippingAddressAssignment $shippingAddressAssignment
+     */
+    public function __construct(
+        QuoteSession $quoteSession,
+        \Magento\Quote\Model\Quote\TotalsCollector $totalsCollector,
+        CustomerRepository $customerRepository,
+        CustomerSession $customerSession,
+        QuoteRepository $quoteRepository,
+        \Magento\Quote\Model\ShippingAddressAssignment $shippingAddressAssignment,
+        \Magento\Customer\Api\AddressRepositoryInterface $addressRepository
+    ) {
+        $this->quoteSession = $quoteSession;
+        $this->totalsCollector = $totalsCollector;
+        $this->customerRepository = $customerRepository;
+        $this->customerSession = $customerSession;
+        $this->quoteRepository = $quoteRepository;
+        $this->shippingAddressAssignment = $shippingAddressAssignment;
+        $this->addressRepository = $addressRepository;
     }
 
     /**
      * If AvaTax GetTaxRequest failed and if configuration is set to prevent checkout, throw exception
      *
      * @param \Magento\Framework\Event\Observer $observer
+     *
      * @return $this
-     * @throws LocalizedException
+     * @throws \Exception
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         /** @var \Magento\Sales\Api\Data\OrderInterface $order */
-        $quote = $this->session->getQuote();
+        $quote = $this->quoteSession->getQuote();
         if (!is_null($quote) && $quote->isVirtual()) {
-            // Only calculate sales tax via this method if the order is virtual so as not to inadvertently raise the
-            // getTax count on physical or hybrid orders
-            
-            // Add logic here to calculate sales tax, possibly just run $quote->collectTotals();
+            $customer = $this->customerRepository->getById($this->customerSession->getCustomerId());
+            $addressId = $customer->getDefaultBilling();
+            /** @var \Magento\Customer\Api\Data\AddressInterface $address */
+            $address = $this->addressRepository->getById($addressId);
+            if ($address !== null) {
+                $address = $quote->getBillingAddress()->importCustomerAddressData($address);
+                $quote->setBillingAddress($address);
+                $this->shippingAddressAssignment->setAddress($quote, $address, false);
+                $quote->setDataChanges(true);
+                $this->quoteRepository->save($quote);
+            }
         }
         return $this;
     }
