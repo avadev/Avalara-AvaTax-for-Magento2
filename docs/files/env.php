adedb2b8dedd88fa90a2d6716fe500588d918338
Erik Hansen <erik@classyllama.com>
2018-12-12 11:19:37 -0600

    Add env.php file for example in the documentation

---
+++ docs/files/env.php
@@ -0,0 +1,103 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'backend',
+  ),
+  'cert-capture' => 
+  array (
+    'url' => 'https://sbx-api.certcapture.com/v2/auth/get-token',
+    'sdk-url' => 'https://sbx.certcapture.com/gencert2/js',
+    'auth' => 
+    array (
+      'username' => 'example@example.com',
+      'password' => 'Xd9Tm0yxd0GHirbm',
+    ),
+    'client-id' => '40306',
+  ),
+  'db' => 
+  array (
+    'connection' => 
+    array (
+      'indexer' => 
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => 'root',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+        'persistent' => NULL,
+      ),
+      'default' => 
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => 'root',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => '',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'ecc7b7bdf458531890dd688f00es233c',
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'cache' => 
+  array (
+    'frontend' => 
+    array (
+      'default' => 
+      array (
+        'backend' => 'Cm_Cache_Backend_Redis',
+        'backend_options' => 
+        array (
+          'server' => 'localhost',
+          'database' => '0',
+          'port' => '6379',
+        ),
+      ),
+    ),
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'target_rule' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 23 May 2018 21:09:30 +0000',
+  ),
+);
