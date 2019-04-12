05f474be2243dde69b4ecda4acb491a5ba3edcd9
Anton Pavelko <a.pavelko@astoundcommerce.com>
2019-04-12 18:19:47 +0300

    Updated composer.json, and app interface for new release

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -20,5 +20,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in composer.json
      */
-    const APP_VERSION = '2.0.1-RC2';
+    const APP_VERSION = '2.0.2-RC1';
 }
