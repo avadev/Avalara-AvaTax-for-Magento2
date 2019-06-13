a4c9860b1de1824f192d8644e8423e5dc0071cde
Mykyta Denysenkov <m.denysenkov@astoundcommerce.com>
2019-06-13 17:35:17 +0300

    Updated version in composer.json and app interface

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -20,5 +20,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in composer.json
      */
-    const APP_VERSION = '2.0.2-RC1';
+    const APP_VERSION = '2.0.3-RC1';
 }
