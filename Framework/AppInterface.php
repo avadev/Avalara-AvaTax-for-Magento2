bf996a45ac91bf0dd95c07c2b32336990a338942
Oleksandr Marynch <o.marynych@astoundcommerce.com>
2020-09-07 16:32:47 +0300

    update version 2.1.8-alpha8

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -20,5 +20,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in composer.json
      */
-    const APP_VERSION = '2.1.8-alpha9';
+    const APP_VERSION = '2.1.8-alpha8';
 }
