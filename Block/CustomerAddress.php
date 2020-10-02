adbec776af1a61415d96034d90e448439e6ac487
marinichsv <marinich@gmail.com>
2020-10-02 14:56:07 +0300

    fixed AVASUP-749: Please, fix constructor in the  CustomerAddress.php class

--- Block/CustomerAddress.php
+++ Block/CustomerAddress.php
@@ -38,8 +38,8 @@ class CustomerAddress extends \Magento\Framework\View\Element\Template
      */
     public function __construct(
         Context $context,
-        array $data = [],
-        Config $config
+        Config $config,
+        array $data = []
     ) {
         parent::__construct($context, $data);
         $this->config = $config;
