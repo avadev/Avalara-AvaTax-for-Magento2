cb439a400091c1da6c8d4d3a7d2fabc3bf5bc305
marinichsv <marinich@gmail.com>
2020-11-16 15:44:57 +0200

    feature AVASUP-798: It`s impossible to place an order.  "regionId" is required

--- Ui/Component/Listing/Column/CrossBorderTypeActions.php
+++ Ui/Component/Listing/Column/CrossBorderTypeActions.php
@@ -77,8 +77,8 @@ class CrossBorderTypeActions extends \Magento\Ui\Component\Listing\Columns\Colum
                             ),
                             'label' => __('Delete'),
                             'confirm' => [
-                                'title' => __('Delete "${ $.$data.title }"'),
-                                'message' => __('Are you sure you wan\'t to delete a "${ $.$data.type }" record?')
+                                'title' => __('Delete %1', $item['type']),
+                                'message' => __('Are you sure you wan\'t to delete a "%1" record?', $item['type'])
                             ]
                         ]
                     ];
