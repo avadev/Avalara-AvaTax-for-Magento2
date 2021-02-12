3857570ca1e25ab8c182848f5290c3f6e383f4a3
Oleksandr Marynch <o.marynych@astoundcommerce.com>
2021-02-12 19:17:23 +0200

    set version 2.2.2-alpha2

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -20,5 +20,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in composer.json
      */
-    const APP_VERSION = '2.2.2-alpha';
+    const APP_VERSION = '2.2.2-alpha2';
 }
