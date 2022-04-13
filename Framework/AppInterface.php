9a2a03f901622131e458637826681b2085a2a687
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2022-04-13 11:26:19 +0300

    Update ext version

--- Framework/AppInterface.php
+++ Framework/AppInterface.php
@@ -20,5 +20,5 @@ interface AppInterface
     /**
      * If this is updated it must also be updated in composer.json
      */
-    const APP_VERSION = '2.2.9';
+    const APP_VERSION = '2.2.10-alpha1';
 }
